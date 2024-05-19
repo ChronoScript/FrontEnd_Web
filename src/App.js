c9df4e1d218896b40d36c6a36d0eeb5d3b3f8811
LakshikaJayarathna <kdlhjayarathna@gmail.com>
2024-05-19 12:25:44 +0530

    SideBar Creation by K.P.G.I.Pathirana

--- src/App.js
+++ src/App.js
@@ -1,44 +1,126 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Menu/Navbar';
+//import Navbar from './Menu/Navbar';
 import UserComponents from './components/UserComponent';
-import HomeComponents from './components/HomeComponent';
-import LoginComponents from './components/LoginComponent';
-import ListHouseOwnerComponents from './components/ListHouseOwnerComponent';
-import HouseOwnerComponents from './components/HouseOwnerComponent';
-import DietitianComponents from './components/DietitianComponent';
-import ListDietitianComponents from './components/ListDietitianComponent';
 
-import {BrowserRouter,Router,Routes,Route,Switch} from 'react-router-dom';
-import HeaderComponent from './components/HeaderComponent';
-import FooterComponent from './components/FooterComponent';
+//import HomeComponents from './components/HomeComponent';
+//import LoginComponents from './components/LoginComponent';
+//import {BrowserRouter,Router,Routes,Route,Switch} from 'react-router-dom';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+
+
+// Filename - App.js
+//import "./App.css";
+import Sidebar from "./components/Sidebar";
+import {
+    AboutUs,
+    OurAim,
+    OurVision,
+} from "./pages/AboutUs";
+import {
+    Services,
+    ServicesOne,
+    ServicesTwo,
+    ServicesThree,
+} from "./pages/Services";
+import {
+    Events,
+    EventsOne,
+    EventsTwo,
+} from "./pages/Events";
+import Contact from "./pages/ContactUs";
+import Support from "./pages/Support";
+import HouseOwnerComponent from './components/HouseOwnerComponent';
+import { LiaStackExchange } from 'react-icons/lia';
+import ListHouseOwnerComponent from './components/ListHouseOwnerComponent';
+import ListDietitianComponent from './components/ListDietitianComponent';
+import DietitianComponent from './components/DietitianComponent';
+import HomeComponent from './components/HomeComponent';
+import LoginComponent from './components/LoginComponent';
+
+
+
+
+
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-         
-          <div className="App">
-            <Switch>
-          
-              <Route path="/Register">   <UserComponents/></Route>
-              <Route path="/Login"> <LoginComponents/></Route>
-              <Route path="/ListHouseOwner"><HeaderComponent/><ListHouseOwnerComponents/><FooterComponent/></Route>
-              <Route path="/HouseOwnerProfile"><HeaderComponent/><HouseOwnerComponents/><FooterComponent/></Route>
-              <Route path="/edit-houseowner/:id"><HeaderComponent/><HouseOwnerComponents/><FooterComponent/></Route>
-              <Route path="/ListDietitian"><HeaderComponent/><ListDietitianComponents/><FooterComponent/></Route>
-              <Route path="/DietitianProfile"><HeaderComponent/><DietitianComponents/><FooterComponent/></Route>
-              <Route path="/edit-dietitian/:id"><HeaderComponent/><DietitianComponents/><FooterComponent/></Route>
-              <Route path="/"> <HomeComponents/></Route>
+      <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={<HomeComponent/>}
+            />
+            <Route
+              path="/login"
+              element={<LoginComponent/>}
+            />
+             <Route
+              path="/register"
+              element={<UserComponents />}
+            />
+          </Routes>
+          <Sidebar />
+          <Routes>
+              <Route
+                  path="about-us"
+                  element={<AboutUs />}
+              />
+              <Route
+                  path="/about-us/aim"
+                  element={<OurAim />}
+              />
+              <Route
+                  path="/about-us/vision"
+                  element={<OurVision />}
+              />
+              <Route
+                  path="/services"
+                  element={<Services />}
+              />
+              <Route
+                  path="/HouseOwnerProfile"
+                  element={<HouseOwnerComponent />}
+              />
+              <Route
+                  path="/ListHouseOwner"
+                  element={<ListHouseOwnerComponent />}
+              />
+              <Route
+                  path="/edit-houseowner/:id"
+                  element={<HouseOwnerComponent />}
+              />
+              <Route
+                  path="/contact"
+                  element={<Contact />}
+              />
+              <Route
+                  path="/ListDietitian"
+                  element={<ListDietitianComponent />}
+              />
+              <Route
+                  path="/DietitianProfile"
+                  element={<DietitianComponent />}
+              />
+              <Route
+                  path="/edit-dietitian/:id"
+                  element={<DietitianComponent />}
+              />
+           
+          </Routes>
+      </Router>
 
 
-
-          
-              </Switch>
-          </div>
-      </BrowserRouter>
-    </div>
   );
 }
+
+
 
 export default App;
